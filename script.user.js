ea2d5eb32585e584ea00df8e39da845b87f72ba7
Pier Carlo Cadoppi <pc.cadoppi@gmail.com>
2023-07-19 15:40:04 +0200

    id is working when clicking

--- script.user.js
+++ script.user.js
@@ -59,21 +59,102 @@
     button.addEventListener("click", () => excludeListing(id));
     return button;
   }
-  // Select all the listing elements
-  const listings = document.querySelectorAll(".SubUnit__Wrapper-sc-10x486s-0");
-  console.log(listings);
 
-  // Iterate over them and append a button to each thumbnail
-  for (const listing of listings) {
-    // Get the listing id from the href attribute
-    const href = listing.getAttribute("href");
-    const id = href.split("/").pop();
-    // Create a button for this listing
-    const button = createButton(id);
-    // Append it to the thumbnail div
-    const thumbnail = listing.querySelector(
-      ".SubUnit__ThumbnailBackdrop-sc-10x486s-2"
-    );
-    thumbnail.appendChild(button);
-  }
+  // Create a mutation observer
+  const observer = new MutationObserver((mutations) => {
+    // Loop through the mutations
+    for (const mutation of mutations) {
+      // Check if any nodes were added
+      if (mutation.addedNodes.length > 0) {
+        // Loop through the added nodes
+        for (const node of mutation.addedNodes) {
+          // Check if the node is the map element
+          if (node.classList.contains("MapSearch__MapWrapper-sc-1ibpnqf-1")) {
+            // Select the map element
+            const map = node;
+            console.log("Map element", map);
+            // Add a click event listener with useCapture option
+            map.addEventListener(
+              "click",
+              () => {
+                console.log("Map clicked");
+
+                // Wait for a short delay to let the listings load
+                setTimeout(() => {
+                  // Select all the listing elements
+                  const mapListing = document.getElementsByClassName(
+                    "SubUnit__Wrapper-sc-10x486s-0"
+                  )[0];
+
+                  if (mapListing) {
+                    console.log("Found map listing:", mapListing);
+
+                    // Get the listing id from the href attribute
+                    const href = mapListing.getAttribute("href");
+                    const id = href.split("/").pop();
+                    console.log("Listing id", id);
+                  }
+
+                  // // Iterate over them and append a button to each thumbnail
+                  // for (const listing of listings) {
+                  //   // Get the listing id from the href attribute
+                  //   const href = listing.getAttribute("href");
+                  //   const id = href.split("/").pop();
+                  //   console.log("Listing id", id);
+                  //   // // Create a button for this listing
+                  //   // const button = createButton(id);
+                  //   // // Append it to the thumbnail div
+                  //   // const thumbnail = listing.querySelector(
+                  //   //   ".SubUnit__ThumbnailBackdrop-sc-10x486s-2"
+                  //   // );
+                  //   // thumbnail.appendChild(button);
+                  // }
+                }, 500); // Adjust the delay as needed
+              },
+              true
+            ); // Set useCapture to true
+            // Disconnect the observer
+            observer.disconnect();
+            // Break the loop
+            break;
+          }
+        }
+      }
+    }
+  });
+
+  // Select the parent element of the map element
+  const parent = document.querySelector(
+    ".SearchPage__MapSearchContainer-gg133s-11"
+  );
+
+  // Start observing the parent element for child node changes
+  observer.observe(parent, { childList: true });
+
+  // console.log(map);
+  // // Add a click event listener
+  // map.on("click", (e) => {
+  //   console.log("Map clicked", e);
+  //   // // Wait for a short delay to let the listings load
+  //   // setTimeout(() => {
+  //   //   // Select all the listing elements
+  //   //   const listings = document.querySelectorAll(
+  //   //     ".SubUnit__Wrapper-sc-10x486s-0"
+  //   //   );
+
+  //   //   // Iterate over them and append a button to each thumbnail
+  //   //   for (const listing of listings) {
+  //   //     // Get the listing id from the href attribute
+  //   //     const href = listing.getAttribute("href");
+  //   //     const id = href.split("/").pop();
+  //   //     // Create a button for this listing
+  //   //     const button = createButton(id);
+  //   //     // Append it to the thumbnail div
+  //   //     const thumbnail = listing.querySelector(
+  //   //       ".SubUnit__ThumbnailBackdrop-sc-10x486s-2"
+  //   //     );
+  //   //     thumbnail.appendChild(button);
+  //   //   }
+  //   // }, 500); // Adjust the delay as needed
+  // });
 })();
