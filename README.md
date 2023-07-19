592e94980cfc014d37312b0f94aa35a2400258a1
Pier Carlo Cadoppi <pc.cadoppi@gmail.com>
2023-07-19 12:58:54 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# daft-ie-exclude-listings
-try to get a place to stay in Ireland!
+# Daft.ie exclude listings
+
+Useful when you are searching for a new place to stay and you found that a listing you have already checked out is not for you. This way you can remove it from the map and only the new listings and the ones you are interested in will appear!
+
+Right now to exclude it you have to modify the code and add the id of the listing to exclude, but I plan to add a button to exclude automatically. You can find the id of the listing in the full URL of the listing, at the end, like this:
+
+<https://www.daft.ie/share/.../1234567>
+
+Where 1234567 is the listing.
---
+++ script.user.js
@@ -0,0 +1,45 @@
+// ==UserScript==
+// @name         Daft.ie exclude listings
+// @namespace    https://github.com/cad0p
+// @version      0.1
+// @description  try to get a place to stay in Ireland!
+// @author       Pier Carlo Cadoppi
+// @match        https://www.daft.ie/property-for-rent/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=daft.ie
+// @grant        none
+// @license      MIT
+// ==/UserScript==
+
+(function () {
+  const listingsToExclude = new Set([5326983]);
+
+  const origOpen = XMLHttpRequest.prototype.open;
+  XMLHttpRequest.prototype.open = function () {
+    this.addEventListener("load", function () {
+      // Only intercept JSON requests.
+      const contentType = this.getResponseHeader("content-type");
+      if (!contentType || !contentType.includes("application/json")) {
+        return;
+      }
+      const res = JSON.parse(this.responseText);
+      if ("listings" in res) {
+        console.log("request completed!");
+        console.log(this.getResponseHeader("content-type"));
+        console.log(res.listings); //whatever the response was
+        // Filter the listings
+        res.listings = res.listings.filter(
+          (l) => !listingsToExclude.has(l.listing.id)
+        );
+        console.log(res.listings);
+        console.log(res);
+        // Override the response text.
+        Object.defineProperty(this, "responseText", {
+          get() {
+            return JSON.stringify(res);
+          },
+        });
+      }
+    });
+    origOpen.apply(this, arguments);
+  };
+})();
